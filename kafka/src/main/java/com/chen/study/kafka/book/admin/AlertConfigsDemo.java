24d9be6fa059151b57ca211c962850dfa43c23c1
陈添明 <CHENTIANMING002@ke.com>
2020-02-25 22:03:31 +0800

    adminClient

--- kafka/src/main/java/com/chen/study/kafka/book/admin/AlertConfigsDemo.java
+++ kafka/src/main/java/com/chen/study/kafka/book/admin/AlertConfigsDemo.java
@@ -1,12 +1,11 @@
 package com.chen.study.kafka.book.admin;
 
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.Config;
-import org.apache.kafka.clients.admin.DescribeConfigsResult;
+import org.apache.kafka.clients.admin.*;
 import org.apache.kafka.common.config.ConfigResource;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
@@ -14,7 +13,7 @@ import java.util.concurrent.ExecutionException;
  * @author 陈添明
  * @date 2020/2/24
  */
-public class DescribeConfigsDemo {
+public class AlertConfigsDemo {
 
     public static final String brokerList = "192.168.199.110:9092,192.168.199.189:9092,192.168.199.242:9092";
     public static final String topic = "topic-admin";
@@ -25,12 +24,14 @@ public class DescribeConfigsDemo {
         props.put(AdminClientConfig.REQUEST_TIMEOUT_MS_CONFIG, 30000);
         AdminClient client = AdminClient.create(props);
 
-        // 描述配置
+        // 修改配置
         ConfigResource resource = new ConfigResource(ConfigResource.Type.TOPIC, topic);
-        DescribeConfigsResult result = client.describeConfigs(Collections.singleton(resource));
-
-        Config config = result.all().get().get(resource);
-        System.out.println(config);
+        ConfigEntry entry = new ConfigEntry("cleanup.policy", "compact");
+        Config config = new Config(Collections.singleton(entry));
+        Map<ConfigResource, Config> configs = new HashMap<>();
+        configs.put(resource, config);
+        AlterConfigsResult result = client.alterConfigs(configs);
+        result.all().get();
 
         client.close();
 
