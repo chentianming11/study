668e5438098f2cffc876fdf976d0a8c0f59f5794
陈添明 <CHENTIANMING002@ke.com>
2020-03-22 20:24:39 +0800

    jvm 内存溢出

---
+++ jvm/src/main/java/com/chen/study/jvm/oom/HeapOOM.java
@@ -0,0 +1,23 @@
+package com.chen.study.jvm.oom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * VM Args：-Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/Users/mac/work/git/study/jvm/heap.hprof
+ *
+ * @author zzm
+ */
+public class HeapOOM {
+
+    static class OOMObject {
+    }
+
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<>();
+
+        while (true) {
+            list.add(new OOMObject());
+        }
+    }
+}
---
+++ jvm/src/main/java/com/chen/study/jvm/oom/JavaMethodAreaOOM.java
@@ -0,0 +1,36 @@
+package com.chen.study.jvm.oom;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * VM Args： -XX:PermSize=10M -XX:MaxPermSize=10M
+ * 1.8并不会抛出异常
+ *
+ * @author zzm
+ */
+public class JavaMethodAreaOOM {
+
+    public static void main(String[] args) {
+        while (true) {
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(OOMObject.class);
+            enhancer.setUseCache(false);
+            enhancer.setCallback(new MethodInterceptor() {
+                @Override
+                public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+                    return proxy.invokeSuper(obj, args);
+                }
+            });
+            enhancer.create();
+        }
+    }
+
+    static class OOMObject {
+
+    }
+}
+
